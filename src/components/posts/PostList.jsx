e98af10bb226288bd65370105159f3b62cbf4148
assal <mohammed1assal23@gmail.com>
2024-07-13 16:09:48 +0100

    new git

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -1,14 +1,23 @@
 import "./posts.css";
 import PostItem from "./PostItem";
 
+
+
 const PostList = ({ posts }) => {
   return (
     <div className="post-list">
       {posts.map((item) => (
         <PostItem key={item._id} post={item} />
       ))}
+      
     </div>
   );
 };
 
 export default PostList;
+
+
+
+
+
+ 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,28 +2,30 @@ import PostList from "../../components/posts/PostList";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { Link } from "react-router-dom";
 import "./home.css";
-import {  categories } from "../../dummyData";
+import { categories } from "../../dummyData";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchPosts } from "../../redux/apiCalls/postApiCall";
 const Home = () => {
   const dispatch = useDispatch();
-const {posts} = useSelector(state => state.post)
+  const { posts } = useSelector((state) => state.post);
   useEffect(() => {
     dispatch(fetchPosts(1));
   }, []);
-  
+
+
+
   return (
     <section className="home">
-    {/*}  <div className="home-hearo-header">
+       <div className="home-hearo-header">
         <div className="home-hero-header-layout">
           <h1 className="home-title">Welcome to english Blog</h1>
         </div>
-  </div>*/}
- {/*  <Sidebar categories={categories} />  */}
+  </div>
+      {/*  <Sidebar categories={categories} />  */}
       <div className="home-latest-post">Latest Posts</div>
       <div className="home-container">
-        <PostList posts={posts} />
+       {!posts ? <> <h1>  wait</h1></>   : <PostList posts={posts} /> }  
         {/* <Sidebar categories={categories} /> */}
       </div>
       <div className="home-see-posts-link">
